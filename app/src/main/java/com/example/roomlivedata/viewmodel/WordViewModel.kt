48390c5488a633b17acabd281cc71d2567ebf7e6
jay huang <jay.huang@wdtl.com>
2020-09-24 17:24:43 +1200

    make original runs

--- app/src/main/java/com/example/roomlivedata/viewmodel/WordViewModel.kt
+++ app/src/main/java/com/example/roomlivedata/viewmodel/WordViewModel.kt
@@ -19,7 +19,7 @@ class WordViewModel(application: Application) : AndroidViewModel(application) {
     val allWords: LiveData<List<Word>>
 
     init {
-        val wordsDao = WordRoomDatabase.getDatabase(application).wordDao()
+        val wordsDao = WordRoomDatabase.getDatabase(application, uiScope).wordDao()
 
         repository = WordRepository(wordsDao)
         allWords = repository.allWords
